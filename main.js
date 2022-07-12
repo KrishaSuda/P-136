d0ef3994e68e64e0578b57c4983d5a5bcdebe611
KrishaSuda <85116052+KrishaSuda@users.noreply.github.com>
2022-07-12 16:36:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video = "";
+status = "";
+input = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting Objects";
+    input = document.getElementById('input_1').value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
